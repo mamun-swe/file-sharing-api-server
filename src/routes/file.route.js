9d37de7bc235a609f73f092d6fa12f76b5a7a039
Mamun-swe <mhmamun166009@gmail.com>
2022-07-17 21:14:34 +0600

    Request limit added for store new file

--- src/routes/file.route.js
+++ src/routes/file.route.js
@@ -3,7 +3,7 @@ const fileRouter = require("express").Router()
 const fileController = require("../controllers/file.controller")
 const { requestLimit } = require("../middlewares/requestlimit.middleware")
 
-fileRouter.post("/", fileController.Store)
+fileRouter.post("/", requestLimit, fileController.Store)
 fileRouter.get("/:publicKey", requestLimit, fileController.Index)
 fileRouter.delete("/:privateKey", fileController.Destroy)
 
